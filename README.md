436ed0bb415e70f5252be901abf3097e4314b60e
Max Kostikov <max@kostikov.co>
2018-08-26 23:19:20 +0200

    Add Bludit 3.x compatibility

---
+++ README.md
@@ -0,0 +1,19 @@
+Yandex Tools
+============
+
+This Bludit CMS plugin generate the meta tag to validate your site with Yandex Webmasters Tools and the JavaScript code to track with Yandex.Metrika
+
+Versions
+--------
+
+**v0.2.2 @ 26/08/2018**
+- (i) add Bludit 3.x compatibility
+
+**v0.2.2 @ 21/11/2017**
+- (i) code cleanup
+
+**v0.2.1 @ 1/11/2017**
+- (i) move on Bludit-like version enumeration
+
+...
+
---
+++ plugin.php
@@ -0,0 +1,82 @@
+<?php
+
+class pluginYandexTools extends Plugin {
+
+	public function init()
+	{
+		$this->dbFields = array(
+			'yametrika-id'=>'',
+			'yandex-verification'=>''
+		);
+	}
+
+	public function form()
+	{
+		global $Language;
+
+		$html  = '<div>';
+		$html .= '<label for="yaverification">'.$Language->get('Yandex.Webmaster').'</label>';
+		$html .= '<input id="yaverification" type="text" name="yandex-verification" value="'.$this->getValue('yandex-verification').'">';
+		$html .= '<span class="tip">'.$Language->get('complete-this-field-with-the-yandex-verification').'</span>';
+		$html .= '</div>';
+
+		$html .= '<div>';
+		$html .= '<label for="yametrika">'.$Language->get('Yandex.Metrika Counter ID').'</label>';
+		$html .= '<input id="yametrika" type="text" name="yametrika-id" value="'.$this->getValue('yametrika-id').'">';
+		$html .= '<span class="tip">'.$Language->get('complete-this-field-with-the-yametrika-id').'</span>';
+		$html .= '</div>';
+
+		return $html;
+	}
+
+	public function siteHead()
+	{
+                global $url;
+
+                if( $this->getValue('yandex-verification') && $url->whereAmI()=='home' ) {
+	                $html   = PHP_EOL.'<!-- Yandex.Webmaster ID -->'.PHP_EOL;
+        	        $html  .= '<meta name="yandex-verification" content="'.$this->getValue('yandex-verification').'" />'.PHP_EOL;
+                	return $html;
+		}
+		return false;
+
+	}
+
+	public function siteBodyEnd()
+	{
+		
+	if( $this->getValue('yametrika-id') ) {
+		$html  = PHP_EOL.'<!-- Yandex.Metrika counter -->'.PHP_EOL;
+		$html .= "<script type='text/javascript'>
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter".$this->getValue('yametrika-id')." = new Ya.Metrika({
+                    id:".$this->getValue('yametrika-id').",
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName('script')[0],
+            s = d.createElement('script'),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'https://mc.yandex.ru/metrika/watch.js';
+
+        if (w.opera == '[object Opera]') {
+            d.addEventListener('DOMContentLoaded', f, false);
+        } else { f(); }
+    })(document, window, 'yandex_metrika_callbacks');
+</script>
+<noscript><div><img src='https://mc.yandex.ru/watch/".$this->getValue('yametrika-id')."' style='position:absolute; left:-9999px;' alt='' /></div></noscript>".PHP_EOL;
+			return $html;
+		}
+
+		return false;
+	}
+}
+
